d311e56d204984ea125698aed7b130d59b999fe7
Marysia <borkowska.maria07@gmail.com>
2025-05-08 15:09:14 +0200

    feat: login function

---
+++ OneDrive/Pulpit/demo/demo/frontend/src/Login.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+
+const Login = ({ onLogin }) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const response = await fetch('http://localhost:8080/auth/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            credentials: 'include',
+            body: JSON.stringify({ email, password }),
+        });
+
+        if (response.ok) {
+            setMessage("Login successful");
+            onLogin();
+        } else {
+            setMessage("Invalid credentials");
+        }
+    };
+
+
+    return (
+        <div>
+            <h2>Login</h2>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button type="submit">Login</button>
+            </form>
+            <p>{message}</p>
+        </div>
+    );
+};
+
+export default Login;

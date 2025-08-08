af6441aa00f0e1a949538f011495d7465a9ee622
Marysia <borkowska.maria07@gmail.com>
2025-08-08 12:51:49 +0200

    fix:adjust class name

---
+++ OneDrive/Pulpit/demo/demo/src/main/java/com/example/demo/RecipePlanner.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipePlanner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipePlanner.class, args);
+	}
+
+}

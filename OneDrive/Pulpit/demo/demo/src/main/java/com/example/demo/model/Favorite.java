fbe64eaab56b6a20a14c824183acc8da38e5d215
Marysia <borkowska.maria07@gmail.com>
2025-08-26 15:14:20 +0200

    feat: Favorite model

---
+++ OneDrive/Pulpit/demo/demo/src/main/java/com/example/demo/model/Favorite.java
@@ -0,0 +1,47 @@
+package com.example.demo.model;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "favorites")
+public class Favorite {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "recipe_id", nullable = false)
+    private Recipe recipe;
+
+
+    public Favorite() {}
+
+    public Favorite(User user, Recipe recipe) {
+        this.user = user;
+        this.recipe = recipe;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+}

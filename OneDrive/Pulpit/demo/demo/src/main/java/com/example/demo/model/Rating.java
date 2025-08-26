3a5ce99ffd81d16b4fe26767c47a6c0c4760e934
Marysia <borkowska.maria07@gmail.com>
2025-08-26 15:15:44 +0200

    feat: Rating model

---
+++ OneDrive/Pulpit/demo/demo/src/main/java/com/example/demo/model/Rating.java
@@ -0,0 +1,50 @@
+package com.example.demo.model;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "ratings")
+public class Rating {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "recipe_id", nullable = false)
+    private Recipe recipe;
+    @Column(name = "rating", nullable = false)
+    private double rating;
+
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+}

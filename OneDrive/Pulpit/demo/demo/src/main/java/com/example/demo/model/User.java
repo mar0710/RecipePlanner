033bb3191ebc7eaa84d22d329394fcc35de8bc5b
Marysia <borkowska.maria07@gmail.com>
2025-08-26 15:17:34 +0200

    fix: added colums annotation

--- OneDrive/Pulpit/demo/demo/src/main/java/com/example/demo/model/User.java
+++ OneDrive/Pulpit/demo/demo/src/main/java/com/example/demo/model/User.java
@@ -1,44 +1,57 @@
 package com.example.demo.model;
-
-import java.util.Objects;
-
 import jakarta.persistence.*;
 
 @Entity
 @Table(name="users")
 public class User {
-
-    private @Id @GeneratedValue Long id;
-    private String userName;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "username", nullable = false)
+    private String username;
+    @Column(name = "email", nullable = false)
     private String email;
+    @Column(name = "password", nullable = false)
     private String password;
+    @Column(name = "role", nullable = false)
+    private String role;
+
 
     public User() {
 
     }
-    public User(String userName, String email, String password) {
+    public User(String userName, String email, String password,String role) {
 
-        this.userName = userName;
+        this.username = userName;
         this.email = email;
         this.password = password;
+        this.role =role;
     }
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     public Long getId() { return id; }
+
     public void setId(Long id) { this.id = id; }
-    @Column(name = "username", nullable = false)
-    public String getUserName() { return userName; }
-    public void setUserName(String name) { this.userName = name; }
-    @Column(name = "email", nullable = false)
+
+    public String getUsername() { return username; }
+
+    public void setUsername(String name) { this.username = name; }
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
-    @Column(name = "password", nullable = false)
+
     public String getPassword() { return password; }
+
     public void setPassword(String password) { this.password = password; }
+    public String getRole() { return role;}
+    public void setRole(String role) { this.role = role; }
 
     @Override
     public String toString() {
-        return "User{" + "id=" + this.id + ", name='" + this.userName + '\'' + '\'' + '}';
+        return "User{" + "id=" + this.id + ", name='" + this.username + '\'' + '\'' + '}';
     }
 
 }
+
+
+
+
+

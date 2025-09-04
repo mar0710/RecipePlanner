f37185e8e168a203162007ffdb80c33e8ef90052
mar0710 <116458991+mar0710@users.noreply.github.com>
2025-09-04 18:41:15 +0200

    fix: update AppNavbar.js

--- OneDrive/Pulpit/demo/demo/frontend/src/AppNavbar.js
+++ OneDrive/Pulpit/demo/demo/frontend/src/AppNavbar.js
@@ -1,13 +1,18 @@
-import React, {Component} from 'react';
-import { Navbar, Nav, NavItem, NavLink } from 'reactstrap';
-import { Link } from 'react-router-dom';
-
-
+import React, { Component } from "react";
+import {
+    Navbar,
+    NavbarToggler,
+    Collapse,
+    Nav,
+    NavItem,
+    NavLink
+} from "reactstrap";
+import { Link } from "react-router-dom";
 
 class AppNavbar extends Component {
     constructor(props) {
         super(props);
-        this.state = {isOpen: false};
+        this.state = { isOpen: false };
         this.toggle = this.toggle.bind(this);
     }
 
@@ -19,54 +24,64 @@ class AppNavbar extends Component {
 
     render() {
         return (
-            <div className={"AppNavbar"}>
-                <Navbar expand="md" className="custom-navbar">
+            <div className="AppNavbar">
+                <Navbar expand="md" className="custom-navbar" fixed="top">
                     <img
-                        src='/favicon.svg'
-                        padding-right='20px'
-                        height='30'
-                        alt=''
-                        loading='lazy'
+                        src="/favicon.svg"
+                        style={{ paddingRight: "20px" }}
+                        height="30"
+                        alt=""
+                        loading="lazy"
                     />
-                    <Nav className="me-auto" navbar>
-                        <NavItem>
-                            <NavLink tag={Link} to="/" className="nv-link">
-                            Home
-                            </NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink tag={Link} to="/recipes" className="nv-link">
-                                Recipes
-                            </NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink tag={Link} to="/recipes/upload" className="nv-link">
-                                Add recipe
-                            </NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink tag={Link} to="/planner" className="nv-link">
-                                Planner
-                            </NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink tag={Link} to="/shoppinglist" className="nv-link">
-                                ShoppingList
-                            </NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink tag={Link} to="/profile" className="nv-link">
-                                Profile
-                            </NavLink>
-                        </NavItem>
-                    </Nav>
-                    <Nav className="ms-auto" navbar>
-                        <NavItem>
-                            <NavLink href="#" onClick={this.props.onLogout} className="nav-link">
-                                Logout
-                            </NavLink>
-                        </NavItem>
-                    </Nav>
+    
+                    <NavbarToggler onClick={this.toggle} />
+    
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="me-auto" navbar>
+                            <NavItem>
+                                <NavLink tag={Link} to="/" className="nv-link">
+                                    Home
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink tag={Link} to="/recipes" className="nv-link">
+                                    Recipes
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink tag={Link} to="/recipes/upload" className="nv-link">
+                                    Add recipe
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink tag={Link} to="/planner" className="nv-link">
+                                    Planner
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink tag={Link} to="/shoppinglist" className="nv-link">
+                                    ShoppingList
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink tag={Link} to="/profile" className="nv-link">
+                                    Profile
+                                </NavLink>
+                            </NavItem>
+                        </Nav>
+
+                        <Nav className="ms-auto" navbar>
+                            <NavItem>
+                                <NavLink
+                                    href="#"
+                                    onClick={this.props.onLogout}
+                                    className="nav-link"
+                                >
+                                    Logout
+                                </NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
                 </Navbar>
             </div>
         );

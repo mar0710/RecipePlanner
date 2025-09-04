d43831d35c145ba553d95c558c879d6bb710d138
mar0710 <116458991+mar0710@users.noreply.github.com>
2025-09-04 18:26:13 +0200

    feat: api configutration

---
+++ OneDrive/Pulpit/demo/demo/frontend/src/api.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import AuthService from './AuthService';
+
+const api = axios.create({
+    baseURL: "http://localhost:8080/",
+});
+
+api.interceptors.request.use(config => {
+    const token = AuthService.getToken();
+    if (token) {
+        config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+});
+
+export default api;
